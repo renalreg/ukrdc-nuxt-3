379adf490718f295af8b36b3af098de4d3736070
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-11-24 09:45:05 +0000

    Fix checkpill model

--- components/base/BaseCheckpill.vue
+++ components/base/BaseCheckpill.vue
@@ -6,7 +6,7 @@ or an array of values when multiple instances are v-model'd onto the same array.
 <template>
   <label class="wrapper flex items-center font-medium text-gray-500">
     <input
-      v-model="proxyChecked"
+      v-model="model"
       class="peer sr-only"
       :class="{ 'checkpill-disabled': disabled }"
       type="checkbox"
@@ -22,17 +22,13 @@ or an array of values when multiple instances are v-model'd onto the same array.
 <script lang="ts">
 
 export default defineComponent({
-  model: {
-    prop: "checked",
-    event: "change",
-  },
   props: {
     label: {
       type: String,
       required: false,
       default: null,
     },
-    checked: {
+    modelValue: {
       type: [Array, Boolean],
       default: false,
     },
@@ -53,14 +49,14 @@ export default defineComponent({
     },
   },
   setup(props, { emit }) {
-    const proxyChecked = computed({
-      get: () => props.checked,
+    const model = computed({
+      get: () => props.modelValue,
       set: (value) => {
-        emit("change", value);
+        emit("update:modelValue", value);
       },
     });
 
-    return { proxyChecked };
+    return { model };
   },
 });
 </script>
