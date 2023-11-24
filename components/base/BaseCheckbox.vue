357976183546d50af69020cf849304983c15a4c8
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-11-24 09:42:26 +0000

    Fix checkbox model

--- components/base/BaseCheckbox.vue
+++ components/base/BaseCheckbox.vue
@@ -7,7 +7,7 @@ or an array of values when multiple instances are v-model'd onto the same array.
   <label class="wrapper flex items-center font-medium text-gray-500">
     {{ rightToLeft ? label : "" }}
     <input
-      v-model="proxyChecked"
+      v-model="model"
       class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
       :class="{ 'opacity-50': disabled, 'mr-2': !rightToLeft, 'ml-2': rightToLeft }"
       type="checkbox"
@@ -21,17 +21,13 @@ or an array of values when multiple instances are v-model'd onto the same array.
 <script lang="ts">
 
 export default defineComponent({
-  model: {
-    prop: "checked",
-    event: "change",
-  },
   props: {
     label: {
       type: String,
       required: false,
       default: null,
     },
-    checked: {
+    modelValue: {
       type: [Array, Boolean],
       default: false,
     },
@@ -52,14 +48,14 @@ export default defineComponent({
     },
   },
   setup(props, { emit }) {
-    const proxyChecked = computed({
-      get: () => props.checked,
+    const model = computed({
+      get: () => props.modelValue,
       set: (value) => {
-        emit("change", value);
+        emit("update:modelValue", value);
       },
     });
 
-    return { proxyChecked };
+    return { model };
   },
 });
 </script>
