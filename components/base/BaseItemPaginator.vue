25788ac4d1808e4d788458b2b095bfa95f717985
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-11-24 09:47:03 +0000

    Fix item paginator model

--- components/base/BaseItemPaginator.vue
+++ components/base/BaseItemPaginator.vue
@@ -3,21 +3,21 @@
     <div class="hidden sm:block">
       <p class="text-gray-700">
         {{ itemLabel }}
-        <span class="font-medium">{{ value + 1 }}</span>
+        <span class="font-medium">{{ modelValue + 1 }}</span>
         of
         <span class="font-medium">{{ total }}</span>
       </p>
     </div>
     <div class="flex flex-1 justify-between sm:justify-end">
       <button
-        :class="{ invisible: value <= 0 }"
+        :class="{ invisible: modelValue <= 0 }"
         class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 font-medium text-gray-700 hover:bg-gray-50"
         @click="prev()"
       >
         Previous
       </button>
       <button
-        :class="{ invisible: value + 1 >= total }"
+        :class="{ invisible: modelValue + 1 >= total }"
         class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 font-medium text-gray-700 hover:bg-gray-50"
         @click="next()"
       >
@@ -31,12 +31,8 @@
 
 
 export default defineComponent({
-  model: {
-    prop: "value",
-    event: "input",
-  },
   props: {
-    value: {
+    modelValue: {
       type: Number,
       required: false,
       default: 0,
@@ -54,11 +50,11 @@ export default defineComponent({
 
   setup(props, { emit }) {
     function next(): void {
-      emit("input", props.value + 1);
+      emit("update:modelValue", props.modelValue + 1);
     }
 
     function prev(): void {
-      emit("input", props.value - 1);
+      emit("update:modelValue", props.modelValue - 1);
     }
 
     return { next, prev };
