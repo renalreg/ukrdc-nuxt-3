a39c3a6bab9c7c37974d2cead4fcae79ff21a285
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-11-27 11:03:26 +0000

    Fix demomode condition

--- composables/useSensitive.ts
+++ composables/useSensitive.ts
@@ -3,21 +3,21 @@ export default function () {
   const runtimeConfig = useRuntimeConfig()
 
   function sensitive(value: string | undefined): string | undefined {
-    if (value && runtimeConfig.public.demoMode) {
+    if (value && runtimeConfig.public.demoMode !== '0') {
       return value.replace(/./g, "*");
     }
     return value;
   }
 
   function sensitiveNumbers(value: string | undefined): string | undefined {
-    if (value && runtimeConfig.public.demoMode) {
+    if (value && runtimeConfig.public.demoMode !== '0') {
       return value.replace(/\d/g, "0");
     }
     return value;
   }
 
   const sensitiveCssFilter = computed(() => {
-    if (runtimeConfig.public.demoMode) {
+    if (runtimeConfig.public.demoMode !== '0') {
       return "blur(8px)";
     } else {
       return "none";
