bf901ddde094bef0ebf9f6e866d78f290e71c234
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-12-04 13:08:32 +0000

    v0.1.1 (debug output for API base URL)

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -138,6 +138,9 @@ export default function () {
     baseUrl = `${runtimeConfig.public.api.protocol}://${runtimeConfig.public.api.host}`;
   }
 
+  console.log(runtimeConfig.public.api);
+  console.log({ baseUrl });
+
   // Create API instances
   const adminApi = new AdminApi(undefined, baseUrl, apiInstance);
   const dashboardApi = new DashboardApi(undefined, baseUrl, apiInstance);
