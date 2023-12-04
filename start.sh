5a1fd95ed64c0a67b58172730316a12f8681011e
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-12-04 15:37:15 +0000

    Conditionally rename runtime config environment variables

--- start.sh
+++ start.sh
@@ -1,15 +1,42 @@
-# Convert old environment variables to new ones
-export NUXT_PUBLIC_SENTRY_CONFIG_ENVIRONMENT=${DEPLOYMENT_ENV}
+# In Nuxt 3, runtime configs can only be overridden using a special
+# form of environment variable, prefixed with NUXT. 
+# See https://nuxt.com/docs/guide/going-further/runtime-config#typing-runtime-config
+# At build-time, and during development, we use environment variables with different
+# names, see nuxt.config.js for a full list of environment variables used in
+# the runtimeConfig.public object.
+# To keep things consistent, we convert these environment variables to their
+# Nuxt 3 counterparts at runtime, in production.
+# Note: We need to only do this if the variable has actually been defined,
+# otherwise it sets it to an empty value and blanks out the defaults 
+# defined in nuxt.config.js
 
-export NUXT_PUBLIC_DEPLOYMENT_ENV=${DEPLOYMENT_ENV}
-export NUXT_PUBLIC_DEMO_MODE=${DEMO_MODE}
+if [ -n "${DEPLOYMENT_ENV}" ]; then
+    export NUXT_PUBLIC_SENTRY_CONFIG_ENVIRONMENT=${DEPLOYMENT_ENV}
+    export NUXT_PUBLIC_DEPLOYMENT_ENV=${DEPLOYMENT_ENV}
+fi
 
-export NUXT_PUBLIC_API_HOST=${API_HOST}
-export NUXT_PUBLIC_API_PROTOCOL=${API_PROTOCOL}
+if [ -n "${DEMO_MODE}" ]; then
+    export NUXT_PUBLIC_DEMO_MODE=${DEMO_MODE}
+fi
 
-export NUXT_PUBLIC_MANAGE_ACCOUNT_URL=${MANAGE_ACCOUNT_URL}
+if [ -n "${API_HOST}" ]; then
+    export NUXT_PUBLIC_API_HOST=${API_HOST}
+fi
 
-export NUXT_PUBLIC_OKTA_ISSUER=${OAUTH_ISSUER}
-export NUXT_PUBLIC_OKTA_CLIENT_ID=${APP_CLIENT_ID}
+if [ -n "${API_PROTOCOL}" ]; then
+    export NUXT_PUBLIC_API_PROTOCOL=${API_PROTOCOL}
+fi
+
+if [ -n "${MANAGE_ACCOUNT_URL}" ]; then
+    export NUXT_PUBLIC_MANAGE_ACCOUNT_URL=${MANAGE_ACCOUNT_URL}
+fi
+
+if [ -n "${OAUTH_ISSUER}" ]; then
+    export NUXT_PUBLIC_OKTA_ISSUER=${OAUTH_ISSUER}
+fi
+
+if [ -n "${APP_CLIENT_ID}" ]; then
+    export NUXT_PUBLIC_OKTA_CLIENT_ID=${APP_CLIENT_ID}
+fi
 
 node .output/server/index.mjs
