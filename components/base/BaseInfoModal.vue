854e2091b6ef8b192b1851985e1a05ea2b791314
Joel Collins <joel.collins@renalregistry.nhs.uk>
2024-01-23 13:05:50 +0000

    feat: switch to a modal for stats info
    Replaces the old tooltip-based UI for showing stats calculation information to a full scrollable modal. Allows for far more information to be included in a more readable layout. Also improves element spacing within the information.

---
+++ components/base/BaseInfoModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <UTooltip :text="tooltip">
+      <UIcon
+        name="i-heroicons-information-circle"
+        class="h-5 w-5 cursor-pointer text-gray-500 hover:text-gray-800"
+        @click="isOpen = true"
+      />
+    </UTooltip>
+
+    <UModal v-model="isOpen">
+      <slot />
+    </UModal>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    tooltip: {
+      type: String,
+      required: false,
+      default: "Click to show info",
+    },
+  },
+
+  setup() {
+    const isOpen = ref(false);
+
+    return { isOpen };
+  },
+});
+</script>
