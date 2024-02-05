516f4f130861e9023be88349c3ce028b358e3e3b
Joel Collins <joel.collins@renalregistry.nhs.uk>
2024-02-05 15:08:36 +0000

    refactor: use premade and reusable components where available

--- components/base/BaseDateRange.vue
+++ components/base/BaseDateRange.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="flex flex-col gap-4 lg:flex-row">
     <div class="flex flex-none gap-2">
-      <UButton size="sm" :color="lastNDays === 7 ? 'primary' : 'white'" @click="setLastNDays(7)"> Last 7 days </UButton>
-      <UButton size="sm" :color="lastNDays === 30 ? 'primary' : 'white'" @click="setLastNDays(30)">
-        Last 30 days
+      <UButton
+        v-for="preset in presetDateRanges"
+        :key="preset.label"
+        size="sm"
+        :color="isLastNDays(preset.days) ? 'primary' : 'white'"
+        :variant="isLastNDays(preset.days) ? 'outline' : 'solid'"
+        @click="setLastNDays(preset.days)"
+      >
+        {{ preset.label }}
       </UButton>
-      <UButton size="sm" :color="lastNDays === 365 ? 'primary' : 'white'" @click="setLastNDays(365)">
-        Last year
+      <UButton
+        size="sm"
+        :color="showCustom ? 'primary' : 'white'"
+        :variant="showCustom ? 'outline' : 'solid'"
+        @click="showCustom = true"
+      >
+        Custom
       </UButton>
-      <UButton size="sm" :color="lastNDays === 3650 ? 'primary' : 'white'" @click="setLastNDays(3650)">
-        Last 10 years
-      </UButton>
-      <UButton size="sm" :color="showCustom ? 'primary' : 'white'" @click="showCustom = true"> Custom </UButton>
       <UButton variant="outline" size="sm" color="red" @click="clear()"> Clear </UButton>
     </div>
     <div v-show="showCustom" class="flex h-8 w-full flex-1">
@@ -26,39 +33,19 @@
       >
         <template #default="{ inputValue, inputEvents, showPopover }">
           <div class="flex h-full flex-row items-center justify-start" @click="showPopover">
-            <div class="relative h-full w-full flex-grow">
-              <svg
-                class="pointer-events-none absolute mx-2 h-full w-4 text-gray-600"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-              </svg>
-              <input :class="textBoxClasses" :value="inputValue.start" v-on="inputEvents.start" />
-            </div>
-            <span class="m-2 flex-shrink-0">
-              <svg class="h-4 w-4 stroke-current text-gray-600" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-              </svg>
-            </span>
-            <div class="relative h-full w-full flex-grow">
-              <svg
-                class="pointer-events-none absolute mx-2 h-full w-4 text-gray-600"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-              </svg>
-              <input :class="textBoxClasses" :value="inputValue.end" v-on="inputEvents.end" />
-            </div>
+            <UInput
+              icon="i-heroicons-calendar-20-solid"
+              class="relative h-full w-full flex-grow"
+              :value="inputValue.start"
+              v-on="inputEvents.start"
+            />
+            <UIcon name="i-heroicons-arrow-right-20-solid" class="mx-2 h-4 w-4 flex-shrink-0 text-gray-600" />
+            <UInput
+              icon="i-heroicons-calendar-20-solid"
+              class="relative h-full w-full flex-grow"
+              :value="inputValue.end"
+              v-on="inputEvents.end"
+            />
           </div>
         </template>
       </VDatePicker>
@@ -68,6 +55,11 @@
 
 <script lang="ts">
 import { type DateRange, nowString } from "~/helpers/dateUtils";
+
+interface PresetRangeItem {
+  label: string;
+  days: number;
+}
 
 export default defineComponent({
   props: {
@@ -84,8 +76,12 @@ export default defineComponent({
   setup(props, { emit }) {
     const showCustom = ref(false);
 
-    const textBoxClasses =
-      "flex-grow pl-8 pr-2 py-1 w-full h-full bg-white border border-gray-300 rounded-md placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 truncate";
+    const presetDateRanges: PresetRangeItem[] = [
+      { label: "Last 7 days", days: 7 },
+      { label: "Last 30 days", days: 30 },
+      { label: "Last year", days: 365 },
+      { label: "Last 10 years", days: 3650 },
+    ];
 
     function clear(): void {
       showCustom.value = false;
@@ -111,6 +107,10 @@ export default defineComponent({
       emit("update:modelValue", newRange);
     }
 
+    function isLastNDays(days: number): boolean {
+      return lastNDays.value === days;
+    }
+
     // Proxy object for the v-calendar model value.
     // This allows us to use v-calendar model modifiers (https://vcalendar.io/datepicker/basics.html#model-modifiers)
     // specifically v-model.range.string to get a range of ISO formatted dates,
@@ -125,10 +125,11 @@ export default defineComponent({
     });
 
     return {
+      presetDateRanges,
       showCustom,
-      textBoxClasses,
       lastNDays,
       setLastNDays,
+      isLastNDays,
       clear,
       modelValueProxy,
     };
