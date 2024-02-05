6c45b07119f58d60876486acbc64e9d0785f2b89
Joel Collins <joel.collins@renalregistry.nhs.uk>
2024-02-05 15:08:15 +0000

    refactor: use premade icons where available

--- components/base/BaseCollapseHeader.vue
+++ components/base/BaseCollapseHeader.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="flex cursor-pointer items-center" @click="toggle()">
-    <span>
-      <svg
-        v-if="!modelValue"
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-        <path
-          fill-rule="evenodd"
-          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-          clip-rule="evenodd"
-        />
-      </svg>
+    <span class="h-5 w-5">
+      <UIcon v-if="!modelValue" class="h-5 w-5" name="i-heroicons-chevron-right-16-solid" />
+      <UIcon v-else class="h-5 w-5" name="i-heroicons-chevron-down-16-solid" />
     </span>
     <h5>{{ label }}</h5>
   </div>
