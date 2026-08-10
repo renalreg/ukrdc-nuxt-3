bf7db7c1e077b9e2670cfe180f2f0510cece7de2
Alice Morris <amorris66@hotmail.co.uk>
2026-08-10 12:51:17 +0100

    refactor: update satellites.vue to match new satellites endpoint response and present satellites in a simple table with ID & description.

--- app/pages/facilities/[code]/satellites.vue
+++ app/pages/facilities/[code]/satellites.vue
@@ -1,26 +1,35 @@
 <template>
-  <UCard>
-    <template #header>
-      <h2>Satellites</h2>
-    </template>
-    <ul>
-      <li v-for="satellite in satellites" :key="satellite.sourceCode" class="py-4">
-        Source code: {{ satellite.sourceCode }}
-      </li>
-    </ul>
+  <!-- List of satellites for the main unit -->
+  <UCard :ui="{ body: { padding: '' } }">
+    <UTable :data="satellites" :columns="satelliteTableColumns" :ui="ui" />
   </UCard>
 </template>
 
 <script lang="ts">
-import type { CodeMapSchema } from "@ukkidney/ukrdc-axios-ts";
+import type { FacilitySchema  } from "@ukkidney/ukrdc-axios-ts";
 
 export default defineComponent({
   props: {
     satellites: {
-      type: Array as () => CodeMapSchema[],
+      type: Array as () => FacilitySchema[],
       required: true,
     }
   },
+  setup() {  
+    const satelliteTableColumns = [
+      { accessorKey: "id", header: "Id" },
+      { accessorKey: "description", header: "Name" },
+    ];
 
+    const ui = {
+      th: { base: "px-6 py-3" },
+      td: { base: "px-6 py-4 whitespace-nowrap" },
+    };
+
+    return {
+      satelliteTableColumns,
+      ui,
+    };
+  }
 });
 </script>
